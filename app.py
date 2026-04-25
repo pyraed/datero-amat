e8f9b8cee6db7c56ca326dceaa9d84c775208287
Agustin Etcheto <a.etcheto@hotmail.com>
2026-04-25 12:56:43 -0300

    Posicion de firma2

--- app.py
+++ app.py
@@ -173,8 +173,8 @@ def enviar_a_firma(pdf_bytes, nombre, dni, telefono):
                 "name": nombre,
                 "dni": dni,
                 "phone": telefono,
-                "position_x": 200,
-                "position_y": 120,
+                "position_x": 180,
+                "position_y": 90,
                 "page": 1
             }
         ]
